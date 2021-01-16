072795cea5344c430b94b45e96876760e10827aa
keshav52 <73228599+keshav52@users.noreply.github.com>
2021-01-17 05:08:40 +0530

    Final App

--- app/src/main/java/com/example/timetableapp/MainActivity.java
+++ app/src/main/java/com/example/timetableapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.timetableapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mondayClicked(View view) {
+        startActivity(new Intent(this,MondayActivity.class));
+    }
+
+    public void tuesdayClicked(View view) {
+        startActivity(new Intent(this,TuesdayActivity.class));
+    }
+
+    public void wednesdayClicked(View view) {
+        startActivity(new Intent(this,WednesdayActivity.class));
+    }
+
+    public void thursdayClicked(View view) {
+        startActivity(new Intent(this, ThursdayActivity.class));
+    }
+
+    public void fridayClicked(View view) {
+        startActivity(new Intent(this,FridayActivity.class));
+    }
+
+    public void saturdayClicked(View view) {
+        startActivity(new Intent(this,SaturdayActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/timetableapp/TuesdayActivity.java
@@ -0,0 +1,14 @@
+package com.example.timetableapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TuesdayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuesday);
+    }
+}
